3053490f32e4eb968a2b5dc1fcc7e843c1457e26
Raffy Banks <raffybanks@gmail.com>
2016-05-07 06:54:59 -0700

    Solve for middle right solutions

--- app.rb
+++ app.rb
@@ -87,11 +87,35 @@ def middle_left_solutions(numbers)
   results
 end
 
+def middle_right_solutions(numbers)
+  results = []
+  ops = ['*', '+', '/', '-']
+  num_perms = numbers.permutation(4).to_a.uniq
+
+  num_perms.each do |nums|
+    ops.each do |op|
+      ops.each do |op2|
+        ops.each do |op3|
+          middle_result = nums[1].send(op2.to_sym, nums[2].to_f)
+          right_result = middle_result.send(op3.to_sym, nums[3].to_f)
+          result = nums[0].send(op.to_sym, right_result.to_f)
+          if result == 24
+            results << "#{nums[0]} #{op} ((#{nums[1]} #{op2} #{nums[2]}) #{op3} #{nums[3]})"
+          end
+        end
+      end
+    end
+  end
+
+  results
+end
+
 def solve(numbers_orig)
   results = []
   results << chain_solutions(numbers_orig.map(&:to_i))
   results << split_solutions(numbers_orig.map(&:to_i))
   results << middle_left_solutions(numbers_orig.map(&:to_i))
+  results << middle_right_solutions(numbers_orig.map(&:to_i))
   results.flatten.uniq
 end
 
